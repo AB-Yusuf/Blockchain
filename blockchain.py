4c7cb1a55f6850c2510a2854fff4b1fb2b40b895
Maestro <engryusufmayowa@gmail.com>
2019-12-29 18:28:51 +0100

    Implementing keyword arguments

--- blockchain.py
+++ blockchain.py
@@ -1,22 +1,23 @@
 """
 Seeding blockhain with an initial value of 1
 """
-blockchain = [[1]]
+blockchain = []
 
 def get_last_block_details():
     return blockchain[-1]
 
-def add_value(transaction_amount):
+def add_value(transaction_amount, last_transaction=[1]):
     """
     Adding new block to the chain, which contains
     details of previous block
     In this case each block is said to contain just
     a single digit
     """
-    blockchain.append([get_last_block_details(),transaction_amount])
+    blockchain.append([last_transaction, transaction_amount])
     print(blockchain)
-    
-add_value(2)
-add_value(3)
+
+add_value(2) 
+add_value(last_transaction=get_last_block_details(), transaction_amount=3)
+
 
  
