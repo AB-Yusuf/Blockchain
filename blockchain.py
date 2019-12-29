e2f1d78534904b1768329cafc6dfc80d6f3ec5dd
Maestro <engryusufmayowa@gmail.com>
2019-12-29 17:53:03 +0100

    Improving add_value functionality

--- blockchain.py
+++ blockchain.py
@@ -3,17 +3,17 @@ Seeding blockhain with an initial value of 1
 """
 blockchain = [[1]]
 
-def add_value():
+def add_value(transaction_amount):
     """
     Adding new block to the chain, which contains
     details of previous block
     In this case each block is said to contain just
     a single digit
     """
-    blockchain.append([blockchain[-1],2])
+    blockchain.append([blockchain[-1],transaction_amount])
     print(blockchain)
     
-add_value()
-add_value()
+add_value(2)
+add_value(3)
 
  
